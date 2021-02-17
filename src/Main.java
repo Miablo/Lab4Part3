4c1a2eb79363c8489f1fc739845b07bcd75ec8f3
miablo <mvd5044@psu.edu>
2021-02-16 21:00:47 -0500

    add event object to test

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         Toolkit tk = Toolkit.getDefaultToolkit();
         MyAWTEventListener listener = new MyAWTEventListener();
-        tk.addAWTEventListener(listener, 0);
+        tk.addAWTEventListener(listener, AWTEvent.MOUSE_EVENT_MASK);
         MyGUI gui = new MyGUI();
         gui.setSize(400, 300);
         gui.setVisible(true);
--- src/MyAWTEventListener.java
+++ src/MyAWTEventListener.java
@@ -1,9 +1,16 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.AWTEventListener;
 
 public class MyAWTEventListener implements AWTEventListener {
     @Override
     public void eventDispatched(AWTEvent event) {
-        System.out.println(event);
+       // System.out.println(event);
+        Object o = event.getSource();
+        if(o instanceof JTextField){
+            JTextField jtf = (JTextField) o;
+            jtf.setText("87");
+
+        }
     }
 }
