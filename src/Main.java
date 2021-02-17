143979b10ff0cbbc78adc0e359d503d157f2c05c
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2021-02-16 20:38:00 -0500

    Add MyGUI.jar file and main calls it

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
+import gui.MyGUI;
+
 public class Main {
+    public static void main(String[] args){
+        MyGUI gui = new MyGUI();
+        gui.setSize(400, 300);
+        gui.setVisible(true);
+    }
 }
