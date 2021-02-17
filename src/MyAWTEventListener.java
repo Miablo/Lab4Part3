ecea58a1b0fda0027e0056072b93d2c78ac051f6
miablo <mvd5044@psu.edu>
2021-02-16 21:07:47 -0500

    Added click listener

--- src/MyAWTEventListener.java
+++ src/MyAWTEventListener.java
@@ -7,10 +7,16 @@ public class MyAWTEventListener implements AWTEventListener {
     public void eventDispatched(AWTEvent event) {
        // System.out.println(event);
         Object o = event.getSource();
-        if(o instanceof JTextField){
-            JTextField jtf = (JTextField) o;
-            jtf.setText("87");
+        int ID = event.getID();
 
-        }
+        System.out.println("Event ID: " + ID);
+       if(ID == 502 || ID == 501) {
+           if(o instanceof JTextField){
+               JTextField jtf = (JTextField) o;
+               jtf.setText("87");
+
+           }
+       }
+
     }
 }
