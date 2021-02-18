a4facd70667fd7184bc4eae6b0cac431ff8ba23f
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2021-02-17 20:18:02 -0500

    Add text file will need to change file path to open location add file in program as input.txt one fails when it should not.

--- src/MyAWTEventListener.java
+++ src/MyAWTEventListener.java
@@ -1,30 +1,74 @@
+/**
+ * @author Cody Walker
+ * @version 1.0
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.AWTEventListener;
 
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MyAWTEventListener implements AWTEventListener {
-    int test;
+    int open = 0;
+    JTextField  output;
+    int t = 0;
+    ArrayList<Integer> testFile = new ArrayList<Integer>();
+
     @Override
     public void eventDispatched(AWTEvent event) {
-        //System.out.println(event.paramString());
         Object o = event.getSource();
         int ID = event.getID();
-        //System.out.println("Event ID: " + ID);
-        if(ID == 500) {
-            if(o instanceof JTextField){
-                JTextField jtf = (JTextField) o;
-                jtf.setText("87");
-                if(jtf.getY() >= 175){
-                    test = Integer.parseInt(jtf.getText());
+        if ( open != -1) { // calls open file if used for first time
+            openFile();
+            open--;
+        }
+        if (ID == 500 && t < testFile.size()) { //check for click input on mouse and under list size
+            if (o instanceof JTextField) {  // check if click object is text field
+                JTextField jtf = (JTextField) o; // give jtf the field of clicked box
+                jtf.setText(String.valueOf(testFile.get(t)));// prints from list
+                if(jtf.getY() < 175) { // not last box or output box check
+                    t++;
+                } else if (jtf.getY() >= 175) {// last box or output box check
+                  output = jtf;
                 }
-            }else if(o instanceof  JButton){
-                if(test == 0) {
-                    System.out.println("Pass");
-                }else{
-                    System.out.println("Falied");
+            } else if (o instanceof JButton) {
+                if (Integer.parseInt(output.getText()) == testFile.get(t)) { // check if output is same as expected value and prints pass or failed accordingly
+                    System.out.println(testFile.get(t-2) + " + " + testFile.get(t-1) + " = " + testFile.get(t) + " Pass");
+                } else {
+                    System.out.println(testFile.get(t-2) + " + " + testFile.get(t-1) + " = " + testFile.get(t) +" Failed got " + output.getText() );
                 }
+                t++;
             }
         }
 
+    }
+
+    /**
+     * Here to open file and save integer to testFile list.
+     */
+    private void openFile() {
+        FileReader file;
+        try {
+            file = new FileReader("E:\\Sweng431\\Lab4Part3\\.idea\\input.txt"); // will need to change file stream to file location on computer.
+            int i;
+            String temp = ""; //sets empty string
+            while((i = file.read()) != -1) { //reads its char
+                if(Character.isDigit((char) i)) { //check if is number
+                    temp += (char)i; // adds temp
+                }else if (Character.isWhitespace((char)i) && temp != ""){ //check if next is whitespace and temp is not empty
+                    testFile.add(Integer.valueOf(temp)); //adds temp to arraylist
+                    temp = ""; // empty temp out
+                }
+            }
+            if(temp != ""){ // verify last integer is add to array
+                testFile.add(Integer.valueOf(temp));
+            }
+            } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
